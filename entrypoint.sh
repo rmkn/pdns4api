dd530e6eb492aada9a51c6639df8c59d72d346e8
rmkn <bubblun@gmail.com>
2018-08-23 10:06:22 +0900

    add DBFILE

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-echo "select * from domains limit 1;" | sqlite3 /tmp/pdns.db
+KEY=${APIKEY:-pdns}
+DBF=${DBFILE:-/tmp/pdns.db}
+
+echo "select * from domains limit 1;" | sqlite3 $DBF
 if [ $? -ne 0 ]; then
 	curl -o /tmp/schema.sqlite3.sql -SL "https://raw.githubusercontent.com/PowerDNS/pdns/auth-4.1.1/modules/gsqlite3backend/schema.sqlite3.sql"
-	sqlite3 /tmp/pdns.db < /tmp/schema.sqlite3.sql
-	chmod 666 /tmp/pdns.db
-	sed -i -e "s/^api-key=.*$/api-key=${APIKEY}/" /etc/pdns/pdns.conf
+	sqlite3 $DBF < /tmp/schema.sqlite3.sql
+	chmod 666 $DBF
+	sed -i -e "s/^api-key=.*$/api-key=${KEY}/" /etc/pdns/pdns.conf
+	sed -i -e "s/^gsqlite3-database=.*$/gsqlite3-database=${DBF//\//\\/}/" /etc/pdns/pdns.conf
 fi
 
 exec "$@"
