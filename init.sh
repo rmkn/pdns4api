9a26175d186165cd0b2a6e33cbad84407081e35f
rmkn <bubblun@gmail.com>
2018-08-23 19:54:28 +0900

    add log option to init.sh(pdns.conf)

--- init.sh
+++ init.sh
@@ -11,4 +11,22 @@ api-key=pdns
 webserver=yes
 webserver-address=0.0.0.0
 webserver-allow-from=0.0.0.0/0
+# api-logfile   Location of the server logfile (used by the REST API)
+# api-logfile=/var/log/pdns.log
+# disable-syslog        Disable logging to syslog, useful when running inside a supervisor that logs stdout
+# disable-syslog=no
+# log-dns-details       If PDNS should log DNS non-erroneous details
+# log-dns-details=no
+# log-dns-queries       If PDNS should log all incoming DNS queries
+log-dns-queries=yes
+# log-timestamp Print timestamps in log lines
+# log-timestamp=yes
+# logging-facility      Log under a specific facility
+# logging-facility=
+# loglevel      Amount of logging. Higher is more. Do not set below 3
+loglevel=6
+# query-logging Hint backends that queries should be logged
+# query-logging=no
+# edns-subnet-processing        If we should act on EDNS Subnet options
+edns-subnet-processing=yes
 EOS
