8d1aafc4b98ab902d63f4115566922d53b65214e
rmkn <bubblun@gmail.com>
2021-04-26 15:08:39 +0900

    PowerDNS4.4に変更

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,10 @@
 FROM rmkn/centos7
-MAINTAINER rmkn
+LABEL maintainer "rmkn"
 
 RUN yum -y install iproute bind-utils yum-plugin-priorities
 
-RUN curl -o /etc/yum.repos.d/powerdns-auth-41.repo https://repo.powerdns.com/repo-files/centos-auth-41.repo
+WORKDIR /etc/yum.repos.d/
+RUN curl -O https://repo.powerdns.com/repo-files/centos-auth-44.repo
 RUN yum -y install pdns pdns-backend-sqlite
 
 COPY entrypoint.sh /
--- init.sh
+++ init.sh
@@ -4,29 +4,27 @@ sed -i -e "s/^launch=/#launch=/" /etc/pdns/pdns.conf
 
 cat << EOS >> /etc/pdns/pdns.conf
 version-string=anonymous
+
+# sqlite
 launch=gsqlite3
 gsqlite3-database=/tmp/pdns.db
+
+# # bind
+# launch=bind
+# bind-config=/etc/named.conf
+# bind-check-interval=60
+# bind-ignore-broken-records=yes
+
 api=yes
 api-key=pdns
 webserver=yes
 webserver-address=0.0.0.0
 webserver-allow-from=0.0.0.0/0
-# api-logfile   Location of the server logfile (used by the REST API)
-# api-logfile=/var/log/pdns.log
-# disable-syslog        Disable logging to syslog, useful when running inside a supervisor that logs stdout
-# disable-syslog=no
-# log-dns-details       If PDNS should log DNS non-erroneous details
-# log-dns-details=no
-# log-dns-queries       If PDNS should log all incoming DNS queries
-#log-dns-queries=yes
-# log-timestamp Print timestamps in log lines
-# log-timestamp=yes
-# logging-facility      Log under a specific facility
-# logging-facility=
-# loglevel      Amount of logging. Higher is more. Do not set below 3
-loglevel=6
-# query-logging Hint backends that queries should be logged
-# query-logging=no
-# edns-subnet-processing        If we should act on EDNS Subnet options
+
+disable-axfr=yes
+
 edns-subnet-processing=yes
+
+expand-alias=yes
+resolver=8.8.8.8
 EOS
